9f0a4ffeb8fce2d0298b82c5cc1bf3c9fc0c6d26
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-10-10 22:22:41 +0300

    .

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary_color: "#838383",
+        light_gray: "#6E6E6E",
+      },
+    },
+  },
+  plugins: [],
+}
