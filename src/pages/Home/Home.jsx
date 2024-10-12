d98c4262a5111b9734642123242761a5c2e9a29e
Seda <seda.diriker@gmail.com>
2024-10-12 20:41:34 +0300

    .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,10 +11,9 @@ const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [gptResponse, setGptResponse] = useState("");
-  const [selectedLanguage, setselectedLanguage] = useState("English")
+  const [selectedLanguage, setselectedLanguage] = useState("English");
   const settingsRef = useRef(null);
 
-  console.log(searchTerm);
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://cse.google.com/cse.js?cx=45d2ff09083bc5958";
@@ -22,9 +21,13 @@ const Home = () => {
     document.body.appendChild(script);
 
     const observer = new MutationObserver(() => {
-      const searchResultsContainer = document.querySelector(".gsc-results-wrapper-nooverlay");
+      const searchResultsContainer = document.querySelector(
+        ".gsc-results-wrapper-nooverlay"
+      );
       if (searchResultsContainer) {
-        const visible = searchResultsContainer.classList.contains("gsc-results-wrapper-visible");
+        const visible = searchResultsContainer.classList.contains(
+          "gsc-results-wrapper-visible"
+        );
         setIsVisible(visible);
 
         const search = document.getElementById("gsc-i-id1");
@@ -34,9 +37,15 @@ const Home = () => {
     });
 
     const checkForSearchResultsContainer = setInterval(() => {
-      const searchResultsContainer = document.querySelector(".gsc-results-wrapper-nooverlay");
+      const searchResultsContainer = document.querySelector(
+        ".gsc-results-wrapper-nooverlay"
+      );
       if (searchResultsContainer) {
-        observer.observe(searchResultsContainer, { attributes: true, childList: true, subtree: true });
+        observer.observe(searchResultsContainer, {
+          attributes: true,
+          childList: true,
+          subtree: true,
+        });
         clearInterval(checkForSearchResultsContainer);
       }
     }, 100);
@@ -46,7 +55,7 @@ const Home = () => {
     };
 
     return () => {
-      document.body.removeChild(script);
+      // document.body.removeChild(script);
       observer.disconnect();
       clearInterval(checkForSearchResultsContainer);
     };
@@ -56,20 +65,21 @@ const Home = () => {
   const fetchGptResponse = async (term) => {
     if (term) {
       try {
-        const response = await fetch("https://api.openai.com/v1/chat/completions", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${apikey}`
-          },
-          body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [
-              { role: "user", content: term }
-            ],
-            max_tokens: 150
-          })
-        });
+        const response = await fetch(
+          "https://api.openai.com/v1/chat/completions",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${apikey}`,
+            },
+            body: JSON.stringify({
+              model: "gpt-3.5-turbo",
+              messages: [{ role: "user", content: term }],
+              max_tokens: 150,
+            }),
+          }
+        );
         const data = await response.json();
         setGptResponse(data.choices[0].message.content);
       } catch (error) {
@@ -80,32 +90,95 @@ const Home = () => {
 
   useEffect(() => {
     fetchGptResponse("Merhaba");
-  }, []); 
+  }, []);
 
   const updateArrowPosition = () => {
-    const searchArrow = document.querySelector('.search-arrow');
+    const searchArrow = document.querySelector(".search-arrow");
+    const form = document.querySelector(".gsc-search-box"); // Form elemanını al
+
     if (searchArrow) {
-      if (selectedEngine === "global-search" && showSettings) {
-        searchArrow.style.top = '13%'; // Hem global-search hem de ayar menüsü açıkken %13
-      } else if (selectedEngine === "global-search") {
-        searchArrow.style.top = '48%'; // Sadece global-search seçiliyse %48
-      } else if (showSettings) {
-        searchArrow.style.top = '27.5%'; // Sadece ayar menüsü açıkken %27.5
-      } else {
-        searchArrow.style.top = '48%'; // Diğer durumlarda %48
-      }
-      console.log("Current arrow position:", searchArrow.style.top);
+        if (selectedEngine === "global-search") {
+            if (form) {
+                form.classList.add("hidden"); 
+            }
+            searchArrow.style.top = "12%";
+        } else {
+            if (form) {
+                form.classList.remove("hidden");
+            }
+            // Eğer showSettings true ise
+            if (showSettings) {
+                searchArrow.style.top = "27.5%"; // Ayar menüsü açıkken %12
+            } else {
+                searchArrow.style.top = "48%"; // Diğer durumlarda %48
+            }
+        }
+        
+        console.log("Current arrow position:", searchArrow.style.top);
     }
+};
+
+async function translateWithGPT(query) {
+  const apiKey = `${apikey}`; 
+  const url = 'https://api.openai.com/v1/chat/completions';
+
+  const data = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `Translate this phrase: "${query}" to ${selectedLanguage}. Just return the translated text.`,
+      },
+    ],
+    max_tokens: 100,
   };
-  
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+
+    if (result.choices && result.choices.length > 0) {
+      const translatedText = result.choices[0].message.content.trim();
+      console.log(`Çevrilen metin: ${translatedText}`);
+
+      const searchUrl = `https://www.google.com/cse?cx=45d2ff09083bc5958&q=${encodeURIComponent(
+        translatedText
+      )}`;
+      window.open(searchUrl, "_self");
+    } else {
+      console.error("Çeviri alınamadı:", result);
+    }
+  } catch (error) {
+    console.error("API isteği sırasında hata:", error);
+  }
+}
+
 
   useEffect(() => {
-    updateArrowPosition(); 
+    updateArrowPosition();
   }, [showSettings, selectedEngine]);
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    if (selectedEngine === "global-search") {
+      translateWithGPT(searchTerm);
+    }
+  };
   return (
     <div className="flex flex-col bg-white w-[100%] mx-auto h-[100vh]">
-      <main className={`search-main flex-grow p-4 ${isVisible ? "mt-[-5rem]" : "mt-20"}`}>
+      <main
+        className={`search-main flex-grow p-4 ${
+          isVisible ? "mt-[-5rem]" : "mt-20"
+        }`}
+      >
         <div>
           <div className="w-full">
             <img
@@ -115,33 +188,73 @@ const Home = () => {
               alt="Logo"
             />
           </div>
-          <div className={`search-container`}
-               onMouseEnter={() => {
-                setShowSettings(true); 
+          {selectedEngine === "global-search" ? (
+            <div
+              className={`search-container`}
+              onMouseEnter={() => {
+                setShowSettings(true);
                 updateArrowPosition(); // Ayar menüsü açıldığında oku günceller
               }}
               onMouseLeave={() => {
-                setShowSettings(false); 
+                setShowSettings(false);
                 updateArrowPosition(); // Ayar menüsü kapandığında oku eski pozisyonuna getirir
-              }} 
-          >
-            <div className="gcse-search">
-              <div id="gsc-i-id1" className="relative"></div>
-            </div>
-            <div className="search-arrow"></div>
+              }}
+            >
+              <div className="gcse-search relative">
+              </div>
+              <form onSubmit={handleSearchSubmit}>
+                <input
+                  type="text"
+                  className=" global-input"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button type="submit">Search</button>
+              </form>
+              <div className="search-arrow"></div>
 
-            {/* SettingsComponent will be shown here */}
-            {showSettings && (
-              <SettingsComponent
-                ref={settingsRef}
-                selectedEngine={selectedEngine}
-                setSelectedEngine={setSelectedEngine}
-                showSettings={showSettings}
-                selectedLanguage={selectedLanguage}
-                setSelectedLanguage={setselectedLanguage}
-              />
-            )}
-          </div>
+              {/* SettingsComponent will be shown here */}
+              {showSettings && (
+                <SettingsComponent
+                  ref={settingsRef}
+                  selectedEngine={selectedEngine}
+                  setSelectedEngine={setSelectedEngine}
+                  showSettings={showSettings}
+                  selectedLanguage={selectedLanguage}
+                  setSelectedLanguage={setselectedLanguage}
+                />
+              )}
+            </div>
+          ) : (
+            <div
+              className={`search-container`}
+              onMouseEnter={() => {
+                setShowSettings(true);
+                updateArrowPosition(); // Ayar menüsü açıldığında oku günceller
+              }}
+              onMouseLeave={() => {
+                setShowSettings(false);
+                updateArrowPosition(); // Ayar menüsü kapandığında oku eski pozisyonuna getirir
+              }}
+            >
+              <div className="gcse-search">
+                <div id="gsc-i-id1" className="relative"></div>
+              </div>
+              <div className="search-arrow"></div>
+
+              {/* SettingsComponent will be shown here */}
+              {showSettings && (
+                <SettingsComponent
+                  ref={settingsRef}
+                  selectedEngine={selectedEngine}
+                  setSelectedEngine={setSelectedEngine}
+                  showSettings={showSettings}
+                  selectedLanguage={selectedLanguage}
+                  setSelectedLanguage={setselectedLanguage}
+                />
+              )}
+            </div>
+          )}
         </div>
 
         {/* GPT Response - only show if selectedEngine is not global-search */}
