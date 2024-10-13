98841279613235b8366b17279f8413cc710d67d7
Seda <seda.diriker@gmail.com>
2024-10-13 21:18:46 +0300

    .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,22 +6,33 @@ import SettingsComponent from "../../components/SettingsComponent";
 import Chatbot from "../../components/Chatbot/Chatbot";
 
 const Home = () => {
-  const [selectedEngine, setSelectedEngine] = useState("google-gpt4"); 
+  const [selectedEngine, setSelectedEngine] = useState("google-gpt4");
   const [showSettings, setShowSettings] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const [cachedResults, setCachedResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [gptResponse, setGptResponse] = useState("");
   const [selectedLanguage, setselectedLanguage] = useState("English");
-  const [searchResults, setSearchResults] = useState([]); 
-  const [imageResults, setImageResults] = useState([]); 
-  const [activeTab, setActiveTab] = useState("web"); 
-
+  const [searchResults, setSearchResults] = useState([]);
+  const [imageResults, setImageResults] = useState([]);
+  const [activeTab, setActiveTab] = useState("web");
+  const [currentPage, setCurrentPage] = useState(1)
   const settingsRef = useRef(null);
   const searchRef = useRef(null);
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(cachedResults.length / itemsPerPage);
 
-console.log(isVisible,"visible")
-console.log(cachedResults)
+  console.log(isVisible, "visible");
+  console.log(imageResults);
+
+
+  const handlePageChange = (newPage) => {
+    if (newPage < 1 || newPage > 10) return; // 1-10 arası sayfa kontrolü
+    fetchSearchResults(searchTerm, newPage); // Yeni sayfayı al
+  };
+  
+  
+  
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://cse.google.com/cse.js?cx=45d2ff09083bc5958";
@@ -36,12 +47,11 @@ console.log(cachedResults)
         const isCurrentlyVisible = searchResultsContainer.classList.contains(
           "gsc-results-wrapper-visible"
         );
-    
-        // Sadece durum değiştiğinde güncelleyin
+
         if (isCurrentlyVisible !== isVisible) {
           setIsVisible(isCurrentlyVisible);
         }
-    
+
         const search = document.getElementById("gsc-i-id1");
         if (search) {
           const searchTerm = search.value;
@@ -52,10 +62,9 @@ console.log(cachedResults)
         }
       }
     });
-    
+
     // Observer'ı uygun öğe üzerinde başlatmayı unutmayın
     observer.observe(document, { childList: true, subtree: true });
-    
 
     const checkForSearchResultsContainer = setInterval(() => {
       const searchResultsContainer = document.querySelector(
@@ -112,19 +121,26 @@ console.log(cachedResults)
     fetchGptResponse("Merhaba");
   }, []);
 
-  const fetchSearchResults = async (query) => {
+  const fetchSearchResults = async (query, page) => {
     if (!query) {
       console.error("Arama terimi boş.");
-      return; 
+      return;
     }
+  
+    // Her sayfa için başlangıç indexini hesapla
+    const start = (page - 1) * 10 + 1;
+  
     try {
-      const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API_KEY}&cx=${process.env.REACT_APP_GOOGLE_CX}&q=${query}`);
+      const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API_KEY}&cx=${process.env.REACT_APP_GOOGLE_CX}&q=${query}&start=${start}`
+      );
       const data = await response.json();
   
       if (data.items) {
-        setSearchResults(data.items); 
+        setSearchResults(data.items);
         setCachedResults(data.items);
-        localStorage.setItem('searchResults', JSON.stringify(data.items));
+        setCurrentPage(page); // Aktif sayfayı ayarla
+        localStorage.setItem("searchResults", JSON.stringify(data.items));
       } else {
         console.error("No results found");
       }
@@ -132,18 +148,21 @@ console.log(cachedResults)
       console.error("Error fetching search results:", error);
     }
   };
+  
   const fetchImageResults = async (query) => {
     if (!query) {
       console.error("Görsel arama terimi boş.");
       return;
     }
     try {
-      const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API_KEY}&cx=${process.env.REACT_APP_GOOGLE_CX}&q=${query}&searchType=image`);
+      const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API_KEY}&cx=${process.env.REACT_APP_GOOGLE_CX}&q=${query}&searchType=image`
+      );
       const data = await response.json();
       if (data.items) {
+        console.log(data.items);
         setImageResults(data.items);
-        localStorage.setItem('imageResults', JSON.stringify(data.items)); 
-
+        localStorage.setItem("imageResults", JSON.stringify(data.items));
       } else {
         console.error("No image results found");
       }
@@ -151,27 +170,23 @@ console.log(cachedResults)
       console.error("Error fetching image results:", error);
     }
   };
-  
-  
-  
+
   const handleSearchSubmit = async (e) => {
     e.preventDefault();
     setShowSettings(false);
-    setIsVisible(true)
+    setIsVisible(true);
     if (selectedEngine === "global-search") {
       const translated = await translateWithGPT(searchTerm);
       if (translated) {
         await fetchSearchResults(translated);
-        await fetchImageResults(translated); 
+        await fetchImageResults(translated);
         setIsVisible(true);
       } else {
         console.error("Çeviri başarısız, arama yapılmadı.");
       }
     }
-    console.log("çalıştı-handle")
+    console.log("çalıştı-handle");
   };
-  
-  
 
   const updateArrowPosition = () => {
     const searchArrow = document.querySelector(".search-arrow");
@@ -199,7 +214,7 @@ console.log(cachedResults)
   const translateWithGPT = async (query) => {
     const apiKey = `${apikey}`;
     const url = "https://api.openai.com/v1/chat/completions";
-  
+
     const data = {
       model: "gpt-3.5-turbo",
       messages: [
@@ -210,7 +225,7 @@ console.log(cachedResults)
       ],
       max_tokens: 100,
     };
-  
+
     try {
       const response = await fetch(url, {
         method: "POST",
@@ -220,9 +235,9 @@ console.log(cachedResults)
         },
         body: JSON.stringify(data),
       });
-  
+
       const result = await response.json();
-  
+
       if (result.choices && result.choices.length > 0) {
         const text = result.choices[0].message.content.trim();
         console.log(`Çevrilen metin: ${text}`);
@@ -237,53 +252,52 @@ console.log(cachedResults)
       return null; // Hata durumunda null döndür
     }
   };
-  
-
 
   useEffect(() => {
     updateArrowPosition();
   }, [showSettings, selectedEngine]);
-
-
 
   const handleLogoClick = () => {
     const searchInput = document.getElementById("gsc-i-id1");
     if (searchInput) {
       searchInput.value = "";
     }
-  
-    const searchResultsContainer = document.querySelector(".gsc-results-wrapper-nooverlay");
-    if (searchResultsContainer && searchResultsContainer.classList.contains("gsc-results-wrapper-visible")) {
-      searchResultsContainer.classList.remove("gsc-results-wrapper-visible"); 
+
+    const searchResultsContainer = document.querySelector(
+      ".gsc-results-wrapper-nooverlay"
+    );
+    if (
+      searchResultsContainer &&
+      searchResultsContainer.classList.contains("gsc-results-wrapper-visible")
+    ) {
+      searchResultsContainer.classList.remove("gsc-results-wrapper-visible");
     }
-  
+
     setGptResponse("");
-    setIsVisible(false); 
+    setIsVisible(false);
   };
 
-
   useEffect(() => {
-    const storedTerm = localStorage.getItem('searchTerm');
+    const storedTerm = localStorage.getItem("searchTerm");
     if (storedTerm) {
       setSearchTerm(storedTerm);
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('searchTerm', searchTerm);
+    localStorage.setItem("searchTerm", searchTerm);
   }, [searchTerm]);
   useEffect(() => {
-    const storedResults = localStorage.getItem('searchResults');
+    const storedResults = localStorage.getItem("searchResults");
     if (storedResults) {
-        setCachedResults(JSON.parse(storedResults));
+      setCachedResults(JSON.parse(storedResults));
     }
 
-    const storedImages = localStorage.getItem('imageResults');
+    const storedImages = localStorage.getItem("imageResults");
     if (storedImages) {
-        setImageResults(JSON.parse(storedImages));
+      setImageResults(JSON.parse(storedImages));
     }
-}, []);
-
+  }, []);
 
   return (
     <div className="flex flex-col bg-white w-[100%] mx-auto h-[100vh]">
@@ -304,30 +318,30 @@ console.log(cachedResults)
           </div>
           {selectedEngine === "global-search" ? (
             <div
-            style={{zIndex:"423432535"}}
-              className={`search-container`}
+              style={{ zIndex: "423432535" }}
+              className={`search-container ${isVisible ? "fixed left-[25%]": "relative"}`}
               onMouseEnter={() => {
-                  setShowSettings(true);
-                  updateArrowPosition(); 
+                setShowSettings(true);
+                updateArrowPosition();
               }}
               onMouseLeave={() => {
-                  setShowSettings(false);
-                  updateArrowPosition(); 
+                setShowSettings(false);
+                updateArrowPosition();
               }}
             >
               <div className="gcse-search relative"></div>
               <form onSubmit={handleSearchSubmit}>
                 <input
                   type="text"
-                  className={`global-input absolute top-1 left-0 z-20 mt-[-3px] w-[100%] ${isVisible ? "py-[10px] px-2" : "py-1 px-2"}`}
+                  className={`global-input absolute top-5 ${isVisible ? "fixed top-28 left-0" : ""} left-0 z-20 mt-[-3px] ${
+                    isVisible ? "py-1 px-2" : "py-1 px-2"
+                  }`}
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
-                
                 />
                 <button type="submit"></button>
               </form>
               <div onClick={handleSearchSubmit} className="search-arrow"></div>
-           
 
               {/* SettingsComponent will be shown here */}
               {showSettings && !isVisible && (
@@ -340,18 +354,108 @@ console.log(cachedResults)
                   setSelectedLanguage={setselectedLanguage}
                 />
               )}
+              {selectedEngine === "global-search" && isVisible && (
+                <div className="search-results mx-auto">
+                  <div className="tabs flex gap-3 text-[#666666] text-[13px]">
+                    <button
+                      className={`tab-button ${
+                        activeTab === "web" ? "active" : ""
+                      }`}
+                      onClick={() => setActiveTab("web")}
+                    >
+                      Web
+                    </button>
+                    <button
+                      className={`tab-button ${
+                        activeTab === "images" ? "active" : ""
+                      }`}
+                      onClick={() => setActiveTab("images")}
+                    >
+                      Görsel
+                    </button>
+                  </div>
+                  <hr />
+
+                  {activeTab === "web" && (
+                    <>
+                      <ul>
+                        {cachedResults.map((result) => (
+                          <li key={result.link}>
+                            <a
+                              className="href-link"
+                              href={result.link}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              dangerouslySetInnerHTML={{
+                                __html: result.htmlTitle,
+                              }}
+                            ></a>
+                            <p className="result-link py-1">
+                              {result.displayLink}{" "}
+                              <span className="text-[10px]">{"›"}</span>
+                            </p>
+                            <p
+                              className="text-[13px]"
+                              dangerouslySetInnerHTML={{
+                                __html: result.htmlSnippet,
+                              }}
+                            ></p>
+                          </li>
+                        ))}
+                      </ul>
+                      <div id="pagination" className="text-[12px] flex gap-2 text-[#666666]">
+  {[...Array(10)].map((_, index) => (
+    <button 
+      key={index + 1} 
+      onClick={() => handlePageChange(index + 1)} 
+      className={`page-button ${currentPage === index + 1 ? "active" : ""}`}
+    >
+      {index + 1}
+    </button>
+  ))}
+</div>
+
+
+                      <div id="results"></div>
+                    </>
+                  )}
+
+                  {activeTab === "images" && (
+                    <>
+                      <ul>
+                        {imageResults.map((imgResult) => (
+                          <li key={imgResult.link}>
+                            <a
+                              href={imgResult.link}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <img
+                                src={imgResult.link}
+                                alt={imgResult.title}
+                                style={{ width: "120px", height: "auto" }}
+                              />
+                              {imgResult.title}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </>
+                  )}
+                </div>
+              )}
             </div>
           ) : (
             <div
               className={`search-container`}
               onMouseEnter={() => {
-                console.log("çalıştı")
+                console.log("çalıştı");
                 setShowSettings(true);
-                updateArrowPosition(); 
+                updateArrowPosition();
               }}
               onMouseLeave={() => {
                 setShowSettings(false);
-                updateArrowPosition(); 
+                updateArrowPosition();
               }}
             >
               <div className="gcse-search">
@@ -373,68 +477,7 @@ console.log(cachedResults)
             </div>
           )}
         </div>
-        <div>
-        {selectedEngine === "global-search" && isVisible && (
-        <div className="search-results">
-          <div className="tabs flex gap-3 text-[#666666] text-[13px]">
-            <button
-              className={`tab-button ${activeTab === "web" ? "active" : ""}`}
-              onClick={() => setActiveTab("web")}
-            >
-              Web 
-            </button>
-            <button
-              className={`tab-button ${activeTab === "images" ? "active" : ""}`}
-              onClick={() => setActiveTab("images")}
-            >
-              Görsel 
-            </button>
-            
-          </div>
-          <hr />
-
-          {activeTab === "web" && (
-            <>
-              <ul>
-                {cachedResults.map((result) => (
-                  <li key={result.link}>
-                    <a className="href-link" href={result.link} target="_blank" rel="noopener noreferrer"                       dangerouslySetInnerHTML={{ __html: result.htmlTitle }}
->
-                    </a>
-                    <p className="result-link py-1">
-                      {result.displayLink} <span className="text-[10px]">{"›"}</span>
-                    </p>
-                    <p
-                      className="text-[13px]"
-                      dangerouslySetInnerHTML={{ __html: result.htmlSnippet }}
-                    ></p>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-
-          {activeTab === "images" && (
-            <>
-              <ul>
-                {imageResults.map((imgResult) => (
-                  <li key={imgResult.link}>
-                    <a href={imgResult.link} target="_blank" rel="noopener noreferrer">
-                      <img
-                        src={imgResult.link}
-                        alt={imgResult.title}
-                        style={{ width: "120px", height: "auto" }}
-                      />
-                      {imgResult.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-        </div>
-      )}
-        </div>
+        <div></div>
         {/* GPT Response - only show if selectedEngine is not global-search */}
         {selectedEngine !== "global-search" && (
           <div className="gpt-response">
